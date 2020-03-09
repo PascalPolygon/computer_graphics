583aa4a81fa022f1784bf98c0ba2406d42eeba96
PascalPolygon <pdao2015@my.fit.edu>
2020-03-09 00:42:23 -0400

    Random obstacle genertion and improved vector field computation algorithm

--- collision_free_path_HW4/collision_free_path.py
+++ collision_free_path_HW4/collision_free_path.py
@@ -1,6 +1,10 @@
 import numpy as np
+# from numba import jit, cuda  # To execute on GPU
+# from numba import vectorize
 import matplotlib.pyplot as plt
 from vpython import*
+import random
+# from numbapro import vectorize
 scene = canvas()
 # sphere()
 
@@ -9,20 +13,11 @@ def computeCost(X, Y, x_g, y_g):
     return np.sqrt(np.square(X-x_g) + np.square(Y-y_g))
 
 
-def computeGradient(X, Y, x_g, y_g):
-    C = computeCost(X, Y, x_g, y_g)
-    dx = (X-x_g)/C
-    dy = (Y-y_g)/C
-    return dx, dy
-
-
 def euclDist(x1, y1, x2, y2):
     return np.sqrt(np.square(x2-x1) + np.square(y2-y1))
 
 
 def gradWithObst(X, Y, x_g, y_g, Obs, R):
-    # dx = np.zeros([100, 100])
-    # dy = np.zeros([100, 100])
     d_g = euclDist(X, Y, x_g, y_g)  # euclidian distance to goal point
     dx = (X-x_g)/d_g
     dy = (Y-y_g)/d_g
@@ -32,30 +27,10 @@ def gradWithObst(X, Y, x_g, y_g, Obs, R):
         d_o = euclDist(X, Y, x_o, y_o)  # euclidian distance to obstacle
         dx += 0 if d_o > R else -((X-x_o)/d_o)
         dy += 0 if d_o > R else -((Y-y_o)/d_o)
-        # dy = 0+dy if d_o > R else ((Y-y_g)/d_g) - ((Y-y_o)/d_o)
-        # dx += ((X-x_g)/d_g) - ((X-x_o)/d_o)
-        # dy += ((Y-y_g)/d_g) - ((Y-y_o)/d_o)
-        # print("Opencountry")
     return dx, dy
 
 
-def gradDescent(x, y, x_g, y_g):
-    C = computeCost(x, y, x_g, y_g)
-    print("Initial Cost: %f" % C)
-    lmbda = 0.1
-    i = 0
-    while C > 0.02:
-        dx, dy = computeGradient(x, y, x_g, y_g)
-        x = x - lmbda*dx
-        y = y - lmbda*dy
-        C = computeCost(x, y, x_g, y_g)
-        print("Epoch %d, cost: %f" % (i, C))
-        i += 1
-    print("Done!")
-    print("Minimum at: %d, %d" % (x, y))
-    return x, y
-
-
+# @jit(target="cuda")  # Decorator for GPU acceleration
 def costWithObstables(X, Y, Obs, R):
     # obsField = np.zeros([100, 100])
     C_obs = 0
@@ -73,40 +48,43 @@ def animGradDesc(x, y, x_g, y_g, obs, R):
     C_obs = costWithObstables(x, y, obs, R)
     C_total = C + C_obs
 
-    print("Initial Cost: %f" % C_total)
+    # print("Initial Cost: %f" % C_total)
     lmbda = 0.1
     i = 0
     # 0,0 in animation is not a corner but the center -50 corrects that
     ball = sphere(pos=vector(x-50, 10, y-50), radius=1,
                   color=color.red, make_trail=True)
-    floor = box(pos=vector(0, 0, 0), size=vector(
+
+    goal = sphere(pos=vector(x_g-50, 10, y_g-50), radius=1,
+                  color=color.green, make_trail=True)
+    box(pos=vector(0, 0, 0), size=vector(
         100, 0.5, 100), color=color.orange)
+
     ball.velocity = vector(0, 0, 0)
 
     for i in range(0, obs.shape[0]):
-        obstacle = cylinder(pos=vector(
-            obs[i][0]-50, 0, obs[i][0]-50), axis=vector(0, 20, 0), radius=R)
+        cylinder(pos=vector(
+            obs[i][0]-50, 0, obs[i][1]-50), axis=vector(0, 20, 0), radius=R)
 
     print("Finding minimum...")
-    while C_total > 0.02:
+    while C_total > 0.05:
         rate(70)
-        # dx, dy = computeGradient(x, y, x_g, y_g)
+
         dx, dy = gradWithObst(x, y, x_g, y_g, obs, R)
-        # ball.velocity = vector(dx, dy, 0)
-        # ball.pos = ball.pos-ball.velocity*lmbda
+
         x = x - lmbda*dx
         y = y - lmbda*dy
+
         ball.pos = vector(x-50, 10, y-50)
-        print(ball.pos)
+        print("x: %f, y: %f" % (ball.pos.x+50, ball.pos.z+50))
 
         C = computeCost(x, y, x_g, y_g)
         C_obs = costWithObstables(x, y, obs, R)
         C_total = C + C_obs
 
-        # print("Epoch %d, cost: %f" % (i, C))
         i += 1
     print("Done!")
-    print("Minimum at: %d, %d" % (x, y))
+    print("Minimum at: %f, %f" % (x, y))
     return x, y
 
 
@@ -114,35 +92,21 @@ def main():
     Grid = np.mgrid[0:100, 0:100]
     X = Grid[0]
     Y = Grid[1]
-    # Arb goal at 78, 93
-    print(X[78][0])
-    print(Y[0][93])
+
     x_g = X[78][0]
     y_g = Y[0][93]
-    # obstacles
-    obs = np.array([52, 53])
-    obs = np.reshape(obs, (1, 2))
 
-    # C = computeCost(X, Y, x_g, y_g)
+    obs = np.array([[random.randint(0, 70), random.randint(0, 85)]])
 
-    # plt.contourf(X, Y, C, cmap='jet')
-    # plt.colorbar()
-    # plt.title('Cost Field')
-    # plt.show()
+    print(obs.shape)
 
-    # dx, dy = computeGradient(X, Y, x_g, y_g)
-    # dx = -dx
-    # dy = -dy
-    # fig, ax = plt.subplots()
-    # ax.contourf(X, Y, C, cmap='jet')
-    # # fig.colorbar()
-    # ax.quiver(X, Y, dx, dy)
-    # ax.set(aspect=1, title='Gradient vector field')
+    for i in range(0, 14):
+        x_o = random.randint(0, 100)
+        y_o = random.randint(0, 100)
+        obs = np.append(obs, [[x_o, y_o]], axis=0)
 
-    # plt.show()
-
-    # gradDescent(50, 50, x_g, y_g)
-    animGradDesc(30, 30, x_g, y_g, obs, 5)
+    animGradDesc(30, 30, x_g, y_g, obs, 2)
+    del X, Y  # free memory
 
 
 if __name__ == "__main__":
