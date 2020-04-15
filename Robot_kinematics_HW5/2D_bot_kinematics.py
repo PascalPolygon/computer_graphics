806de9bbba4ff973cf06b9e5c35b2ef8ecbae74c
PascalPolygon <pdao2015@my.fit.edu>
2020-04-15 17:40:18 -0400

    Attempted final

--- Robot_kinematics_HW5/2D_bot_kinematics.py
+++ Robot_kinematics_HW5/2D_bot_kinematics.py
@@ -177,7 +177,7 @@ def rotate_frame(robot, deg, deg2, deg3):
 
         robot.p5.pos.x = p5_pos[0]
         robot.p5.pos.y = p5_pos[1]
-        
+
         print('deg: %f -> p5 x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
 
 def main():
@@ -198,13 +198,10 @@ def main():
     p3 = cylinder(pos=vector(
             0, 24.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius) #diameter 2
 
-    p3_y = l1.pos.y+(l1.height/2)+p3.radius*np.sin(90)
-    p3_x = l1.pos.x*np.cos(90)
-    print('L1 length: %f' % l1.height)
-    # print("p2 y: %f" % p2.pos.y)
-    print('p3_y: %f' % p3_y)
-    print('p3_x: %f' % p3_x)
 
+    # print('L1 length: %f' % l1.height)
+    # print("p2 y: %f" % p2.pos.y)
+    
     l2 = box(pos=vector(0, 30.5, 0), size=vector(3,10, 3), color=color.purple) 
 
     p4 = cylinder(pos=vector(
@@ -215,6 +212,10 @@ def main():
     p5 = cylinder(pos=vector(
             0, 48.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius) #diameter 2
 
+    # p5_y = l3.pos.y+(l3.height/2)+p5.radius*np.sin(90)
+    # p5_x = l1.pos.x*np.cos(90)
+    print('p5_y: %f' % p5.pos.y)
+    print('p5_x: %f' % p5.pos.x)
     # origin = sphere(pos=vector(0,0,0), radius=5, color = color.red)
   
     myBot = Robot(p2, l1, p3, l2, p4, l3, p5)
