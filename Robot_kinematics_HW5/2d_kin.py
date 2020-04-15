50200b1f5f5aa7b59e51c4bf427abaee09c6052f
PascalPolygon <pdao2015@my.fit.edu>
2020-04-15 17:19:47 -0400

    Fixed buggy link animation

--- Robot_kinematics_HW5/2d_kin.py
+++ Robot_kinematics_HW5/2d_kin.py
@@ -28,6 +28,25 @@ def cos(deg):
 def sin(deg):
     return np.sin(deg)
 
+def rotate_link(robot, link, deg):
+    deg = -1 * deg
+    if link == 1:
+        y = robot.l1.pos.y
+        axis_y = y - (5+robot.p2.radius)
+        for i in range(0, deg-1, -1):
+            rate(100)
+            # print("deg: %d, rad: %f" % (i, np.radians(i)))
+            robot.l1.rotate(angle=rads(-1),axis=vector(0,0,1), origin=vector(0,axis_y,0))
+            L = robot.l1.height+(2*robot.p3.radius)
+            p3_x = np.dot(L,np.cos(rads(90+i)))
+            p3_y = (robot.p2.pos.y) + np.dot(L, np.sin(rads(90+i)))
+            # p3_y = robot.l1.pos.y+(robot.l1.height/2)+robot.p3.radius
+            # print('deg: %f ->P3 x: %f, y: %f' % (i, p3_x, p3_y))
+            robot.p3.pos.x = p3_x
+            robot.p3.pos.y = p3_y
+            # robot.p3.y = robot.l1.pos.y+(robot.l1.height/2)+robot.p3.radius*np.sin(i)
+
+
 def get_transformation(angle, dx, dy):
     return np.array([[cos(angle), -1*sin(angle), dx], 
                     [sin(angle), cos(angle), dy],
@@ -73,6 +92,12 @@ def rotate_frame(robot, deg, deg2, deg3):
         # print('deg: %f -> end effector x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
         robot.p3.pos.x = p3_pos[0]
         robot.p3.pos.y = p3_pos[1]
+     
+
+        ######## Animating link2 using trig from p3 reference frame
+        # robot.l2.pos.x = p3_pos[0] + np.dot((robot.l2.height/2+robot.p3.radius), np.cos(rads(90+i)))
+        # robot.l2.pos.y = p3_pos[1] + np.dot((robot.l2.height/2+robot.p3.radius), np.sin(rads(90+i)))
+        # robot.l2.rotate(angle=rads(step), axis=vector(0,0,1))
 
         Tp3_l2 = get_transformation(rads(i), 0,(robot.l2.height/2)+(robot.p4.radius))
         T0_l2 = np.dot(T0_2, Tp3_l2)
@@ -83,7 +108,10 @@ def rotate_frame(robot, deg, deg2, deg3):
 
         robot.p4.pos.x = p4_pos[0]
         robot.p4.pos.y = p4_pos[1]
-
+        # print('deg: %f -> p4 x: %f, y: %f' % (i, p4_pos[0], p4_pos[1]))
+        # robot.l3.pos.x = p4_pos[0] + np.dot((robot.l3.height/2+robot.p4.radius), np.cos(rads(90+i)))
+        # robot.l3.pos.y = p4_pos[1] + np.dot((robot.l3.height/2+robot.p4.radius), np.sin(rads(90+i)))
+        # robot.l3.rotate(angle=rads(step), axis=vector(0,0,1))
         Tp4_l3 = get_transformation(0, 0,(robot.l3.height/2)+(robot.p5.radius))
         T0_l3 = np.dot(T0_3, Tp4_l3)
         l3_pos = np.dot(T0_l3, np.array([0,0,1]))
@@ -93,7 +121,7 @@ def rotate_frame(robot, deg, deg2, deg3):
 
         robot.p5.pos.x = p5_pos[0]
         robot.p5.pos.y = p5_pos[1]
-        print('deg: %f -> p5 x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
+        # print('deg: %f -> p5 x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
     print("### SECOND LOOP ###")
     deg = -1*deg2
     start = robot.theta2
@@ -129,6 +157,10 @@ def rotate_frame(robot, deg, deg2, deg3):
 
         robot.p4.pos.x = p4_pos[0]
         robot.p4.pos.y = p4_pos[1]
+        # print('deg: %f -> p4 x: %f, y: %f' % (i, p4_pos[0], p4_pos[1]))
+        # robot.l3.pos.x = p4_pos[0] + np.dot((robot.l3.height/2+robot.p4.radius), np.cos(rads(90+i)))
+        # robot.l3.pos.y = p4_pos[1] + np.dot((robot.l3.height/2+robot.p4.radius), np.sin(rads(90+i)))
+        # robot.l3.rotate(angle=rads(step), axis=vector(0,0,1))
 
               ######## Animating link 3 using transformation matrix
         Tp4_l3 = get_transformation(0, 0,(robot.l3.height/2)+(robot.p5.radius))
@@ -140,8 +172,7 @@ def rotate_frame(robot, deg, deg2, deg3):
 
         robot.p5.pos.x = p5_pos[0]
         robot.p5.pos.y = p5_pos[1]
-
-        print('deg: %f -> p5 x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
+        
     print("#### THIRD LOOP ###")
     deg = -1*deg3
     start = robot.theta3
@@ -177,8 +208,7 @@ def rotate_frame(robot, deg, deg2, deg3):
 
         robot.p5.pos.x = p5_pos[0]
         robot.p5.pos.y = p5_pos[1]
-        
-        print('deg: %f -> p5 x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
+        # print('deg: %f -> p5 x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
 
 def main():
     print("Hello world!")
@@ -246,3 +276,45 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# def rotate_frame(robot, cmmds):
+#     for l in range(len(robot)-1):
+#         deg = -1*cmmds[l]
+#         bot = robot[l]
+#         y = bot.l.pos.y
+#         axis_y = y - ((bot.l.height/2)+bot.p.radius)
+#         for i in range(0, deg-1, -1):
+#             rate(100)
+#             bot.l.rotate(angle=rads(-1),axis=vector(0,0,1), origin=vector(0,axis_y,0))
+#             angle1 = rads(i)
+#             T_i = get_transformation(angle1, 0, bot.p.pos.y)
+#             angle2 = rads(0)
+#             T_ii = get_transformation(angle2, 0, bot.l.height+(2*robot[l+1].p.radius))
+#             T = np.dot(T_i, T_ii)
+#             p_i_pos = np.dot(T_i, np.array([0,0,1]))
+#             p_ii_pos = np.dot(T, np.array([0,0,1]))
+#             print('deg: %f ->P3 x: %f, y: %f' % (i, p_i_pos[0], p_ii_pos[1]))
+#             robot[l+1].p.pos.x = p_ii_pos[0]
+#             robot[l+1].p.pos.y = p_ii_pos[1]
+#             robot
+
+  # class Link:
+    #     def __init__(self, p, l):
+    #         self.p = p
+    #         self.l = l
+
+    # Robot = []
+    # Robot.append(Link(p2, l1))
+    # Robot.append(Link(p3, 0))
+    # Robot.append(Link(p4, l3))
+    # Robot.append(Link(p5, 0)) #There is no link 
+
+       ######## Animating link 2 using transformation matrix
+        # Tp3_l2 = get_transformation(rads(i), 0,(robot.l2.height/2)+(robot.p4.radius))
+        # T0_l2 = np.dot(T0_2, Tp3_l2)
+        # l2_pos = np.dot(T0_l2, np.array([0,0,1]))
+        # robot.l2.rotate(angle=rads(-1), axis=vector(0,0,1))
+        # robot.l2.pos.x = l2_pos[0]
+        # robot.l2.pos.y = l2_pos[1]
