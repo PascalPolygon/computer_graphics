d4f222fcb94388d63b8e295a98a3720c69cc9546
PascalPolygon <pdao2015@my.fit.edu>
2020-05-08 23:59:17 -0400

    Obstance avoidance, broken constraint cost

--- Robot_inv_kinematics_HW6/main.py
+++ Robot_inv_kinematics_HW6/main.py
@@ -9,49 +9,52 @@ from linalg_utils import *
 
 scene = canvas()
 
+
 def init_linkage():
     joint_radius = 1
 
     platform = box(pos=vector(0, 0, 0), size=vector(
-        100,3, 100), color=color.orange)
+        100, 3, 100), color=color.orange)
 
-    base =  box(pos=vector(0, 6.5, 0), size=vector(
-        10,10, 10), color=color.orange) #6.5 = 3/2 + 10/2
+    base = box(pos=vector(0, 6.5, 0), size=vector(
+        10, 10, 10), color=color.orange)  # 6.5 = 3/2 + 10/2
 
     p2 = cylinder(pos=vector(
-            0, 12.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius) #diameter 2 
+        0, 12.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius)  # diameter 2
 
     l1 = box(pos=vector(0, 18.5, 0), size=vector(
-        3,10, 3), color=color.purple) #18 = 11.5+2+10/2
+        3, 10, 3), color=color.purple)  # 18 = 11.5+2+10/2
 
     p3 = cylinder(pos=vector(
-            0, 24.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius) #diameter 2
+        0, 24.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius)  # diameter 2
 
-    l2 = box(pos=vector(0, 30.5, 0), size=vector(3,10, 3), color=color.purple) 
+    l2 = box(pos=vector(0, 30.5, 0), size=vector(3, 10, 3), color=color.purple)
 
     p4 = cylinder(pos=vector(
-            0, 36.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius) #diameter 2
+        0, 36.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius)  # diameter 2
 
-    l3 = box(pos=vector(0, 42.5, 0), size=vector(3,10, 3), color=color.purple)
+    l3 = box(pos=vector(0, 42.5, 0), size=vector(3, 10, 3), color=color.purple)
 
     p5 = cylinder(pos=vector(
-            0, 48.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius, color=color.red) #diameter 2
+        0, 48.5, -1.5), axis=vector(0, 0, 3), radius=joint_radius, color=color.red, make_trail=True)  # diameter 2
 
     return p2, l1, p3, l2, p4, l3, p5
 
+
 def jacobian(phi, l):
     delt_i = rads(0.3)
-    de_dphi = np.ndarray((3,len(phi)))
+    de_dphi = np.ndarray((3, len(phi)))
     d_phi = np.zeros(len(phi))
     for i in range(len(phi)):
-        d_phi[i] =  delt_i
-        num1 = e(phi+d_phi,l)
-        num2 = e(phi,l)
-        de_dphi[i]= (num1-num2)/delt_i
+        d_phi[i] = delt_i
+        num1 = e(phi+d_phi, l)
+        num2 = e(phi, l)
+        de_dphi[i] = (num1-num2)/delt_i
 
     de_dphi = de_dphi.transpose()
-    de_dphi = de_dphi[:2,:]
+    de_dphi = de_dphi[:2, :]
     return de_dphi
+
 
 def e(phi, l):
     T0_1 = get_transformation(phi[0], 0, l[0])
@@ -62,9 +65,79 @@ def e(phi, l):
     T0_2 = np.dot(T0_1, T1_2)
     T0_3 = np.dot(T0_2, T2_3)
     T0_4 = np.dot(T0_3, T3_4)
-    v = np.array([0,0,1])
+    v = np.array([0, 0, 1])
 
-    return np.dot(T0_4,v)
+    return np.dot(T0_4, v)
+
+
+# def costWithObstables(X, Y, o, R=1):
+#     print(f'x_o: {o[0]}')
+#     print(f'y_o: {o[1]}')
+#     C_obs = 0
+#     d = computeCost(X, Y, o[0], o[1])
+#     C_obs += 0 if d > R else np.log(R/d)
+#     return C_obs
+
+
+def costWithObstables(X, Y, Obs, R=3):
+    # obsField = np.zeros([100, 100])
+    C_obs = 0
+    for i in range(0, Obs.shape[0]):
+        x_o = Obs[i][0]
+        y_o = Obs[i][1]
+        d = computeCost(X, Y, x_o, y_o)
+        C_obs += 0 if d > R else np.log(R/d)
+
+    return C_obs
+
+
+def cstrntCost(phi, sigma=10):
+    C_cst = np.array([0.0, 0.0, 0.0])
+    _min = -160
+    _max = 160
+    for i in range(len(phi)):
+        if _min < phi[i] and phi[i] <= _min+sigma:
+            C_cst[i] = np.log(sigma/(phi[i]-min))
+            print('###############################')
+            print(f'Angles: {phi}, constraint: {C_cst}')
+        elif _max-sigma <= phi[i] < _max:
+            C_cst[i] = np.log(sigma/(_max - phi[i]))
+            print('###############################')
+            print(f'Angles: {phi}, constraint: {C_cst}')
+    return C_cst
+
+
+def gradDesc(g, e, beta=0.001):
+    return beta*(g-e)
+
+
+def gradWithObst(g, o, e, R=3):
+    # de = gradDesc(g, e)
+    de = gradientDesc(e[0], e[1], g[0], g[1])
+    for i in range(0, o.shape[0]):
+        print(f'O shape: {o.shape}')
+        x_o = o[i][0]
+        y_o = o[i][1]
+        # d_o = euclDist(X, Y, x_o, y_o)  # euclidian distance to obstacle
+        # euclidian distance to obstacle
+        print('Calling')
+        print(x_o)
+        print(y_o)
+        # C_o = costWithObstables(e[0], e[1], o[i])
+        C_o = computeCost(e[0], e[1], x_o, y_o)
+        # do = gradDesc(o[i], e)
+        do = gradientDesc(e[0], e[1], x_o, y_o)
+        # de[0] +=  0 if d_o > R else do
+        print(de)
+        print(do)
+        # de +=  0 if d_o > R else do
+        # de[0] +=  0 if C_o > R else -2*do[0]
+        # de[1] +=  0 if C_o > R else -2*do[1]
+        de += 0 if C_o > R else -do
+        # de[0] += 0 if C_o > R else -((e[0]-x_o)/d_o)
+        # de[1] += 0 if C_o > R else -((e[1]-y_o)/d_o)
+    return de
+
 
 def main():
     print("Hello world!")
@@ -72,47 +145,71 @@ def main():
     p2, l1, p3, l2, p4, l3, p5 = init_linkage()
     myBot = Robot(p2, l1, p3, l2, p4, l3, p5)
     print("Ready to boogy master chief!")
-    x_g = 35
-    y_g = 21
-    goal = sphere(pos=vector(x_g, y_g, -1.5),radius=1, color=color.green)
+    # x_g = 35
+    # y_g = 21
 
     phi1 = 0
     phi2 = 0
     phi3 = 0
-    
+
     # magic_p5 = cylinder(pos=vector(
     #         0, 48.5, -1.5), axis=vector(0, 0, 3), radius=1, color=color.yellow)
 
-    l = np.array([myBot.p2.pos.y, myBot.l1.height+(2*myBot.p3.radius), myBot.l2.height+(2*myBot.p4.radius), myBot.l3.height+(2*myBot.p5.radius)])
+    l = np.array([myBot.p2.pos.y, myBot.l1.height+(2*myBot.p3.radius),
+                  myBot.l2.height+(2*myBot.p4.radius), myBot.l3.height+(2*myBot.p5.radius)])
     phi = np.array([rads(phi1), rads(phi2), rads(phi3)])
     # e_th = np.array([0, 48.5])
     e_c = np.array([0, 48.5])
-    g = np.array([35, 21])
-    beta = 0.001
-    C = computeCost(e_c[0], e_c[1], g[0], g[1])
-    i = 0
+    # g = np.array([35, 21])
+    g = np.array([0, -21])
+    goal = sphere(pos=vector(g[0], g[1], -1.5), radius=1, color=color.green)
 
-    while C > 0.5:
+    # obs = np.array([[25, 31], [30, 19]])
+    obs = np.array([[25, 31], [23, 41]])
+    # obs = np.array([[]])
+    # obs = np.array([[25, 31]])
+    for i in range(obs.shape[0]):
+        sphere(pos=vector(obs[i][0], obs[i][1], -1.5),
+               radius=1, color=color.blue)
+    # obs = np.append(obs, [[o[0], o[1]]], axis=0)
+    # obs = np.array([o])
+    # print(obs[0])
+
+    beta = 0.001
+    i = 0
+    C = computeCost(e_c[0], e_c[1], g[0], g[1])
+    C_obs = costWithObstables(e_c[0], e_c[1], obs)
+    C_total = C+C_obs
+
+    while C_total > 0.5:
         J = jacobian(phi, l)
         J_pinv = np.linalg.pinv(J)
-        de_c = beta*(g-e_c)
+        # de_c = beta*(g-e_c)
         # de_th = beta*(g-e_th)
+        # de_c = gradDesc(g,e_c)
+        de_c = -0.1*gradWithObst(g, obs, e_c)
         d_phi = np.dot(J_pinv, de_c)
-        phi+=d_phi
+        phi += d_phi
         phi_deg = degrees(phi)
         print(f'Go to: {phi_deg}')
         if (i % 20):
             myBot.move(-1*phi_deg[0], -1*phi_deg[1], -1*phi_deg[2])
-        i+= 1
+        i += 1
         e_c = e(phi, l)[:2]
         # e_th+=de_th
         # magic_p5.pos.x = e_c[0]
         # magic_p5.pos.y = e_c[1]
         print(e_c)
         # print(e_th)
-        print('_____________________________')
         C = computeCost(e_c[0], e_c[1], g[0], g[1])
-
+        C_obs = costWithObstables(e_c[0], e_c[1], obs)
+        C_cst = cstrntCost(-1*phi_deg)
+        C_total = C+C_obs
+        print(f'C_total: {C}')
+        print(f'C_obs: {C_obs}')
+        print(f'C_total: {C_total}')
+        print(f'Angles: {-1*phi_deg}, constraint: {C_cst}')
+        print('_____________________________')
 
 
 if __name__ == "__main__":
--- Robot_inv_kinematics_HW6/robot.py
+++ Robot_inv_kinematics_HW6/robot.py
@@ -2,6 +2,7 @@ import numpy as np
 from linalg_utils import *
 from vpython import*
 
+
 class Robot:
     def __init__(self, p2, l1, p3, l2, p4, l3, p5):
         self.p2 = p2
@@ -14,11 +15,12 @@ class Robot:
         self.theta1 = 0
         self.theta2 = 0
         self.theta3 = 0
-    
+
     def move(self, deg, deg2, deg3):
         # angle = rads(-deg)
         print(f'deg: {deg}, deg2: {deg2}, deg3: {deg3}')
-        myStep = 0.003
+        # myStep = 0.003
+        myStep = 0.01
         deg = -1*deg
         axis_y = self.p2.pos.y
         axis_x = self.p2.pos.x
@@ -32,45 +34,52 @@ class Robot:
 
         for i in np.arange(start, deg, step):
             # rate(100)
-            self.l1.rotate(angle=rads(step),axis=vector(0,0,1), origin=vector(axis_x, axis_y,0))
+            self.l1.rotate(angle=rads(step), axis=vector(
+                0, 0, 1), origin=vector(axis_x, axis_y, 0))
             self.theta1 = deg
             angle1 = rads(i)
             T0_1 = get_transformation(angle1, 0, self.p2.pos.y)
             # angle2 = rads(0)
             angle2 = rads(self.theta2)
-            T1_2 = get_transformation(angle2, 0, self.l1.height+(2*self.p3.radius))
+            T1_2 = get_transformation(
+                angle2, 0, self.l1.height+(2*self.p3.radius))
             # angle3 = rads(0)
             angle3 = rads(self.theta3)
-            T2_3 = get_transformation(angle3, 0, self.l2.height+(2*self.p4.radius)) #Really doesn't make a difference p3 or p4 or p5
+            # Really doesn't make a difference p3 or p4 or p5
+            T2_3 = get_transformation(
+                angle3, 0, self.l2.height+(2*self.p4.radius))
             # angle4 = rads(0)
-            T3_4 = get_transformation(rads(0), 0, self.l3.height+(2*self.p5.radius))
+            T3_4 = get_transformation(
+                rads(0), 0, self.l3.height+(2*self.p5.radius))
             T0_2 = np.dot(T0_1, T1_2)
             T0_3 = np.dot(T0_2, T2_3)
             T0_4 = np.dot(T0_3, T3_4)
 
-            p2_pos = np.dot(T0_1,np.array([0,0,1]))
-            p3_pos = np.dot(T0_2, np.array([0,0,1]))
-            p4_pos = np.dot(T0_3, np.array([0,0,1]))
-            p5_pos = np.dot(T0_4, np.array([0,0,1]))
+            p2_pos = np.dot(T0_1, np.array([0, 0, 1]))
+            p3_pos = np.dot(T0_2, np.array([0, 0, 1]))
+            p4_pos = np.dot(T0_3, np.array([0, 0, 1]))
+            p5_pos = np.dot(T0_4, np.array([0, 0, 1]))
 
             # print('deg: %f -> end effector x: %f, y: %f' % (i, p5_pos[0], p5_pos[1]))
             self.p3.pos.x = p3_pos[0]
             self.p3.pos.y = p3_pos[1]
 
-            Tp3_l2 = get_transformation(rads(i), 0,(self.l2.height/2)+(self.p4.radius))
+            Tp3_l2 = get_transformation(
+                rads(i), 0, (self.l2.height/2)+(self.p4.radius))
             T0_l2 = np.dot(T0_2, Tp3_l2)
-            l2_pos = np.dot(T0_l2, np.array([0,0,1]))
-            self.l2.rotate(angle=rads(step), axis=vector(0,0,1))
+            l2_pos = np.dot(T0_l2, np.array([0, 0, 1]))
+            self.l2.rotate(angle=rads(step), axis=vector(0, 0, 1))
             self.l2.pos.x = l2_pos[0]
             self.l2.pos.y = l2_pos[1]
 
             self.p4.pos.x = p4_pos[0]
             self.p4.pos.y = p4_pos[1]
 
-            Tp4_l3 = get_transformation(0, 0,(self.l3.height/2)+(self.p5.radius))
+            Tp4_l3 = get_transformation(
+                0, 0, (self.l3.height/2)+(self.p5.radius))
             T0_l3 = np.dot(T0_3, Tp4_l3)
-            l3_pos = np.dot(T0_l3, np.array([0,0,1]))
-            self.l3.rotate(angle=rads(step), axis=vector(0,0,1))
+            l3_pos = np.dot(T0_l3, np.array([0, 0, 1]))
+            self.l3.rotate(angle=rads(step), axis=vector(0, 0, 1))
             self.l3.pos.x = l3_pos[0]
             self.l3.pos.y = l3_pos[1]
 
@@ -90,34 +99,40 @@ class Robot:
         # print("start: %d, deg: %d" % (start, deg))
         for i in np.arange(start, deg, step):
             # rate(100)
-            self.l2.rotate(angle=rads(step),axis=vector(0,0,1), origin=vector(axis_x, axis_y,0))
+            self.l2.rotate(angle=rads(step), axis=vector(
+                0, 0, 1), origin=vector(axis_x, axis_y, 0))
             self.theta2 = deg
             # angle1 = rads(0)
             T0_1 = get_transformation(angle1, 0, self.p2.pos.y)
             angle2 = rads(i)
-            T1_2 = get_transformation(angle2, 0, self.l1.height+(2*self.p3.radius))
+            T1_2 = get_transformation(
+                angle2, 0, self.l1.height+(2*self.p3.radius))
             # angle3 = rads(0)
             angle3 = rads(self.theta3)
-            T2_3 = get_transformation(angle3, 0, self.l2.height+(2*self.p4.radius)) #Really doesn't make a difference p3 or p4 or p5
+            # Really doesn't make a difference p3 or p4 or p5
+            T2_3 = get_transformation(
+                angle3, 0, self.l2.height+(2*self.p4.radius))
             # angle4 = rads(0)
-            T3_4 = get_transformation(rads(0), 0, self.l3.height+(2*self.p5.radius))
+            T3_4 = get_transformation(
+                rads(0), 0, self.l3.height+(2*self.p5.radius))
             T0_2 = np.dot(T0_1, T1_2)
             T0_3 = np.dot(T0_2, T2_3)
             T0_4 = np.dot(T0_3, T3_4)
 
-            p2_pos = np.dot(T0_1,np.array([0,0,1]))
-            p3_pos = np.dot(T0_2, np.array([0,0,1]))
-            p4_pos = np.dot(T0_3, np.array([0,0,1]))
-            p5_pos = np.dot(T0_4, np.array([0,0,1]))
+            p2_pos = np.dot(T0_1, np.array([0, 0, 1]))
+            p3_pos = np.dot(T0_2, np.array([0, 0, 1]))
+            p4_pos = np.dot(T0_3, np.array([0, 0, 1]))
+            p5_pos = np.dot(T0_4, np.array([0, 0, 1]))
 
             self.p4.pos.x = p4_pos[0]
             self.p4.pos.y = p4_pos[1]
 
-                ######## Animating link 3 using transformation matrix
-            Tp4_l3 = get_transformation(0, 0,(self.l3.height/2)+(self.p5.radius))
+            # Animating link 3 using transformation matrix
+            Tp4_l3 = get_transformation(
+                0, 0, (self.l3.height/2)+(self.p5.radius))
             T0_l3 = np.dot(T0_3, Tp4_l3)
-            l3_pos = np.dot(T0_l3, np.array([0,0,1]))
-            self.l3.rotate(angle=rads(step), axis=vector(0,0,1))
+            l3_pos = np.dot(T0_l3, np.array([0, 0, 1]))
+            self.l3.rotate(angle=rads(step), axis=vector(0, 0, 1))
             self.l3.pos.x = l3_pos[0]
             self.l3.pos.y = l3_pos[1]
 
@@ -138,26 +153,30 @@ class Robot:
         # print("start: %d, deg: %d" % (start, deg))
         for i in np.arange(start, deg, step):
             # rate(100)
-            self.l3.rotate(angle=rads(step),axis=vector(0,0,1), origin=vector(axis_x, axis_y,0))
+            self.l3.rotate(angle=rads(step), axis=vector(
+                0, 0, 1), origin=vector(axis_x, axis_y, 0))
             # print(i)
             self.theta3 = deg
             # angle1 = rads(0)
             T0_1 = get_transformation(angle1, 0, self.p2.pos.y)
             # angle2 = rads(0)
-            T1_2 = get_transformation(angle2, 0, self.l1.height+(2*self.p3.radius))
+            T1_2 = get_transformation(
+                angle2, 0, self.l1.height+(2*self.p3.radius))
             angle3 = rads(i)
-            T2_3 = get_transformation(angle3, 0, self.l2.height+(2*self.p4.radius)) #Really doesn't make a difference p3 or p4 or p5
+            # Really doesn't make a difference p3 or p4 or p5
+            T2_3 = get_transformation(
+                angle3, 0, self.l2.height+(2*self.p4.radius))
             # angle4 = rads(0)
-            T3_4 = get_transformation(rads(0), 0, self.l3.height+(2*self.p5.radius))
+            T3_4 = get_transformation(
+                rads(0), 0, self.l3.height+(2*self.p5.radius))
             T0_2 = np.dot(T0_1, T1_2)
             T0_3 = np.dot(T0_2, T2_3)
             T0_4 = np.dot(T0_3, T3_4)
 
-            p2_pos = np.dot(T0_1,np.array([0,0,1]))
-            p3_pos = np.dot(T0_2, np.array([0,0,1]))
-            p4_pos = np.dot(T0_3, np.array([0,0,1]))
-            p5_pos = np.dot(T0_4, np.array([0,0,1]))
+            p2_pos = np.dot(T0_1, np.array([0, 0, 1]))
+            p3_pos = np.dot(T0_2, np.array([0, 0, 1]))
+            p4_pos = np.dot(T0_3, np.array([0, 0, 1]))
+            p5_pos = np.dot(T0_4, np.array([0, 0, 1]))
 
             self.p5.pos.x = p5_pos[0]
             self.p5.pos.y = p5_pos[1]
-        
